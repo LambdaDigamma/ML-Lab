780c2ea40ad9d17db133f2b7c111a69587d9f435
Marvin Speks <Marvin.Speks@rwth-aachen.de>
2023-10-19 15:36:09 +0200

    Initial Commit

---
+++ Core and Create ML/Core_and_Create_MLApp.swift
@@ -0,0 +1,17 @@
+//
+//  Core_and_Create_MLApp.swift
+//  Core and Create ML
+//
+//  Created by Marvin Speks on 19.10.23.
+//
+
+import SwiftUI
+
+@main
+struct Core_and_Create_MLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
